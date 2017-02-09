05e9a0115e2bbc87e4d853ea7d185e9c95dbe35b
Gauthier Levha <levha.g@gmail.com>
2017-02-09 12:46:27 +0100

    heroku seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@ require 'json'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-url_ingredients = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-ingredients = JSON.parse(open(url_ingredients).read)
+# url_ingredients = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# ingredients = JSON.parse(open(url_ingredients).read)
 
-Ingredient.destroy_all
+# Ingredient.destroy_all
 
-ingredients["drinks"].each do |ingredient|
-	Ingredient.create!(name: ingredient["strIngredient1"].to_s)
-end
+# ingredients["drinks"].each do |ingredient|
+# 	Ingredient.create!(name: ingredient["strIngredient1"].to_s)
+# end
 
 url_cocktails = 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
 cocktails = JSON.parse(open(url_cocktails).read)
