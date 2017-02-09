d9537be4570a2d409457ce0b799c80c69341e733
Gauthier Levha <levha.g@gmail.com>
2017-02-09 18:55:29 +0100

    day 1 over

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,21 +1,14 @@
 class DosesController < ApplicationController
-	before_action :set_cocktail, only: [:new, :create, :destroy]
-	before_action :set_ingredient, only: :create
+	before_action :set_cocktail, only: [:create, :destroy]
 	before_action :set_dose, only: :destroy
-
-	def new
-		@dose = Dose.new
-		@ingredients = Ingredient.all
-	end
 
 	def create
 		@dose = Dose.new(dose_params)
 		@dose.cocktail = @cocktail
-		@dose.ingredient = @ingredient
 		if @dose.save
   		redirect_to cocktail_path(@cocktail)
   	else
-  		render :new
+  		render "cocktails/show"
   	end
 	end
 
@@ -34,11 +27,7 @@ class DosesController < ApplicationController
 		@cocktail = Cocktail.find(params[:cocktail_id])
 	end
 
-	def set_ingredient
-		params[:dose][:ingredient_id] == "" ? nil : @ingredient = Ingredient.find(params[:dose][:ingredient_id])
-	end
-
 	def dose_params
-		params.require(:dose).permit(:description)
+		params.require(:dose).permit(:description, :ingredient_id)
 	end
 end
